2bf6246f5fc0ad784921b41fe142848621481007
BeLeap <beleap@beleap.dev>
2023-11-11 17:38:19 +0900

    fix exported struct

--- internal/lib/ssh.go
+++ internal/lib/ssh.go
@@ -14,12 +14,12 @@ type CustomSsh struct {
 	Session *ssh.Session
 }
 
-type CustomSshError struct {
+type customSshError struct {
 	Stderr   string
 	RawError error
 }
 
-func (e *CustomSshError) Error() string {
+func (e *customSshError) Error() string {
 	b, err := json.Marshal(e)
 	if err != nil {
 		return "Failed to marshal error to json"
@@ -27,7 +27,7 @@ func (e *CustomSshError) Error() string {
 	return string(b)
 }
 
-func (s *CustomSsh) RunCommand(ctx context.Context, command string) (string, *CustomSshError) {
+func (s *CustomSsh) RunCommand(ctx context.Context, command string) (string, *customSshError) {
 	var stdoutBuffer bytes.Buffer
 	var stderrBuffer bytes.Buffer
 	s.Session.Stdout = &stdoutBuffer
@@ -36,7 +36,7 @@ func (s *CustomSsh) RunCommand(ctx context.Context, command string) (string, *Cu
 	tflog.Info(ctx, fmt.Sprintf("Running command \"%s\"", command))
 	err := s.Session.Run(command)
 	if err != nil {
-		return "", &CustomSshError{
+		return "", &customSshError{
 			Stderr:   stderrBuffer.String(),
 			RawError: err,
 		}
