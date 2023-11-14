d4d74b4c9a03fd282e8386f77d3c93de00a3b66a
BeLeap <beleap@beleap.dev>
2023-11-14 15:15:35 +0900

    add example

---
+++ examples/directory/config.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    linux = {
+      source = "beleap/linux"
+    }
+  }
+}
+
+provider "linux" {
+  host        = "node01.titanv.exp.riiid.cloud"
+  username    = "root"
+  private_key = file("../../ssh-keys/id_rsa")
+}
+
+data "linux_directory" "test" {}
