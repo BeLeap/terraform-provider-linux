bf27c13c90a04c0e061322fec2e35524037ec464
BeLeap <beleap@beleap.dev>
2024-01-07 15:39:31 +0900

    Changed ssh connection parameters for test node.

--- internal/util/testFixture.go
+++ internal/util/testFixture.go
@@ -8,13 +8,13 @@ import (
 )
 
 func GetLinuxContext(t *testing.T) LinuxContext {
-	auth, err := goph.UseAgent()
+	auth, err := goph.Key("../../ssh-keys/id_rsa", "")
 	if err != nil {
 		t.Fatalf("Failed to create auth info: %v", err)
 		t.FailNow()
 	}
 
-	sshClient, err := goph.New("root", "test-node.fox-deneb.ts", auth)
+	sshClient, err := goph.New("root", "test-node.fox-deneb.ts.net", auth)
 	if err != nil {
 		t.Fatalf("Failed to connect test node: %v", err)
 		t.FailNow()
